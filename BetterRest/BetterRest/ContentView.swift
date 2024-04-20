f3e95dcfa6d3a4244f254216504d1299ba720381
Sreekutty Maya <sreekuttystar@gmail.com>
2024-04-20 10:56:11 +0400

    date picker swift UI

--- BetterRest/BetterRest/ContentView.swift
+++ BetterRest/BetterRest/ContentView.swift
@@ -9,8 +9,22 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var sleepAmount = 8.0
+    @State private var wakeUp = NSDate.now
+
     var body: some View {
-        Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount,in:4...12,step: 0.25)
+        VStack(alignment:.center) {
+            Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount,in:4...12,step: 0.25)
+            let tomorrow = Date.now.addingTimeInterval(86400)
+
+            // create a range from those two
+            let range = Date.now...tomorrow
+            DatePicker("", selection: $wakeUp,in: Date.now...).labelsHidden()
+            DatePicker("", selection: $wakeUp,in: Date.now...).labelsHidden()
+            DatePicker("", selection: $wakeUp,displayedComponents: .date).labelsHidden()
+            DatePicker("select date", selection: $wakeUp,in: Date.now...)
+
+
+        }.padding()
     }
 }
 
