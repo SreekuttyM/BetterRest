3bb458862d93695a86f0e44b5388e944d7593495
Sreekutty Maya <sreekuttystar@gmail.com>
2024-04-20 10:45:56 +0400

    project set up added

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Sreekutty Maya on 20/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
